d63f442f73e2ebcba63dfe35da8da56881ea9b3a
Oleh Bohak <eddrfgthyjn@gmail.com>
2024-06-24 14:10:40 +0300

    Added faq page

--- scripts.js/swiper.js
+++ scripts.js/swiper.js
@@ -1,6 +1,16 @@
-new Swiper(".swiper-container", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+const parent = document.querySelector(".faq-container");
+
+parent.addEventListener("click", (event) => {
+  const target = event.target;
+  // Перевіряємо, чи натиснуто на елемент з класом "backdrop-heading-container" або його дочірній елемент
+  if (target.closest(".backdrop-heading-container")) {
+    // Знаходимо батьківський елемент ".backdrop-container"
+    const backdropContainer = target.closest(".backdrop-container");
+    // Знаходимо відповідні елементи всередині цього контейнера
+    const bgTitle = backdropContainer.querySelector(".backdrop-text");
+    const arrowIcon = backdropContainer.querySelector(".backdrop-arrow-icon");
+    // Перемикаємо класи
+    bgTitle.classList.toggle("is-active");
+    arrowIcon.classList.toggle("is-rotate");
+  }
 });
