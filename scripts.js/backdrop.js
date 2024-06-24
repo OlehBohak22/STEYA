7752a2514c3948fed9a71a959c5f7672adae77ec
Oleh Bohak <eddrfgthyjn@gmail.com>
2024-06-24 17:57:48 +0300

    Product page already

--- scripts.js/backdrop.js
+++ scripts.js/backdrop.js
@@ -2,14 +2,10 @@ const parent = document.querySelector(".faq-container");
 
 parent.addEventListener("click", (event) => {
   const target = event.target;
-  // Перевіряємо, чи натиснуто на елемент з класом "backdrop-heading-container" або його дочірній елемент
   if (target.closest(".backdrop-heading-container")) {
-    // Знаходимо батьківський елемент ".backdrop-container"
     const backdropContainer = target.closest(".backdrop-container");
-    // Знаходимо відповідні елементи всередині цього контейнера
     const bgTitle = backdropContainer.querySelector(".backdrop-text");
     const arrowIcon = backdropContainer.querySelector(".backdrop-arrow-icon");
-    // Перемикаємо класи
     bgTitle.classList.toggle("is-active");
     arrowIcon.classList.toggle("is-rotate");
   }
